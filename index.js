3dfb8ff6c0c6710889f9c0574f9bf05c92c5a2f3
DevAhmedNaeem <ahmednaeem455e@gmail.com>
2025-08-30 02:59:14 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("Script.js initializing");
+let boxes = document.querySelector(".container").children;
+function getRandomColor() {
+  let val1 = Math.floor(Math.random() * 255);
+  let val2 = Math.floor(Math.random() * 255);
+  let val3 = Math.floor(Math.random() * 255);
+  return `rgb(${val1}, ${val2}, ${val3})`;
+}
+Array.from(boxes).forEach((e) => {
+  let newcolor = getRandomColor();
+  e.style.backgroundColor = newcolor;
+  e.innerHTML = newcolor;
+});
